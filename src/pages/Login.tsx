647e461dd7ec88d07c312b67e897df7b4e4c0f9c
Dong Seunghyeon <kj94gakt@naver.com>
2022-09-04 22:51:48 +0900

    style: Login Page UI

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,30 @@
+/* eslint-disable react/button-has-type */
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Header from '../components/Header';
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Header />
+      <div className="flex float-right w-[42rem] h-[42rem] mr-[10rem]">
+        <img className="MainImage" alt="mainimage" src="images/magic.gif" />
+      </div>
+      <div className="flex w-[55rem] h-[4rem] ml-[5rem] mt-[5rem] font-bold text-6xl">
+        <p>지우고 싶은 이미지</p>
+      </div>
+      <div className="flex w-[55rem] h-[4rem] ml-[5rem] mt-[2rem] font-bold text-6xl">
+        <p>깔끔하게 지워드립니다!</p>
+      </div>
+      <div className="flex justify-center items-center mt-20 font-melon">
+        <Link
+          to="/SignUp"
+          style={{ color: 'inherit', textDecoration: 'inherit' }}
+        >
+          로 그 인
+        </Link>
+      </div>
+    </div>
+  );
 }
