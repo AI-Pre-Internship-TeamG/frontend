851194dd61d818964b1c22011dd059ccb64e4207
Dong Seunghyeon <kj94gakt@naver.com>
2022-09-07 20:57:46 +0900

    chore: 코드 수정

--- src/components/LogoutBtn.tsx
+++ src/components/LogoutBtn.tsx
@@ -5,10 +5,7 @@ export default function LogoutBtn() {
   return (
     <div className="Logout">
       <div className="float-right mt-[4rem] mr-[3rem] font-sds text-4xl">
-        <Link
-          to="/SignUp"
-          style={{ color: 'inherit', textDecoration: 'inherit' }}
-        >
+        <Link to="/" style={{ color: 'inherit', textDecoration: 'inherit' }}>
           로 그 아 웃
         </Link>
       </div>
