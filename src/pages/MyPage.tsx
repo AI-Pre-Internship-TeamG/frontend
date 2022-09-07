b19e877f2ace54908705fcbb7680d8bf2cfb002d
hyunwoo <ashyonu@gmail.com>
2022-09-07 22:08:10 +0900

    feat: add MyPage.tsx

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,5 +1,54 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import Header from '../components/Header';
+import ImgList from '../components/ImgCard';
+import ImgCard from '../components/ImgCard';
 
-export default function MyPage() {
-  return <div>MyPage</div>;
+export interface Data {
+  img_url: string;
+  date: string;
 }
+
+const MyPage = () => {
+  const [datas, setDatas] = useState<Data[]>();
+
+  useEffect(() => {
+    setDatas(example);
+  }, []); 
+
+  return (
+    <>
+      <Header/>
+      {datas?.map(data => <ImgList key={data.img_url} data={data} />)}
+    </>
+  )
+}
+
+export default MyPage;
+
+const example: Data[] = [
+  {
+    img_url: "https://picsum.photos/600/400/?random",
+    date: "2022.08.30"
+  },
+  {
+    img_url: "https://picsum.photos/600/400/?random",
+    date: "2022.09.01"
+  },
+  {
+    img_url: "https://picsum.photos/600/400/?random",
+    date: "2022.09.02"
+  },
+  {
+    img_url: "https://picsum.photos/600/400/?random",
+    date: "2022.09.03"
+  },
+  {
+    img_url: "https://picsum.photos/600/400/?random",
+    date: "2022.09.04"
+  },
+  {
+    img_url: "https://picsum.photos/600/400/?random",
+    date: "2022.09.05"
+  }
+]
