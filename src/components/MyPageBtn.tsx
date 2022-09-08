daf51aeb48850e68ac93ba336187d1f234ab0961
Dong Seunghyeon <kj94gakt@naver.com>
2022-09-08 16:52:06 +0900

    chore: code 수정

--- src/components/MyPageBtn.tsx
+++ src/components/MyPageBtn.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function LogoutBtn() {
+export default function MyPageBtn() {
   return (
-    <div className="Logout">
+    <div className="MyPageBtn">
       <div className="float-right mt-[4rem] mr-[3rem] font-sds text-4xl">
         <Link
           to="/mypage"
