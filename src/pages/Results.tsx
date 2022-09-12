db5a6d0a0b29a2ad6acc2da3d80e6eb5fe03ab05
hyunwoo <ashyonu@gmail.com>
2022-09-12 23:47:18 +0900

    fix: fix esLint Errors

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -3,6 +3,7 @@ import Header from '../components/Header';
 import LogoutBtn from '../components/LogoutBtn';
 import MyPageBtn from '../components/MyPageBtn';
 import Button from '../components/Button';
+
 function useWindowSize() {
   const [size, setSize] = useState([window.innerHeight, window.innerWidth]);
 
@@ -19,9 +20,10 @@ function useWindowSize() {
 }
 
 export default function Results() {
-  const [height, width] = useWindowSize();
+  // const [height, width] = useWindowSize();
   return (
-    <div className='bg-zinc-50' style={{width:width,height:'auto'}}>
+    <div className="bg-zinc-50"> 
+    {/* style={{width: width, height: "auto"}} */}
       <LogoutBtn />
       <MyPageBtn />
       <Header />
@@ -33,12 +35,11 @@ export default function Results() {
           marginBottom: '10px',
         }}
       />
-      <div className='flex items-center justify-items-ceneter flex-col w-full mt-10'  >
-        <img src={"https://via.placeholder.com/160"} style={{display:'flex',width:'40%', borderRadius:'2rem'}} className="image" />
-     
-      <div className='flex flex-row items-center justify-items-center w-1/3' >
-        <Button name= {'다운로드'}/>
-        <Button name = {'공유하기'}/>
+      <div className="flex items-center justify-items-ceneter flex-col w-full mt-10"  >
+        <img src="https://via.placeholder.com/160" alt="이미지" style={{display:"flex", width:"40%", borderRadius:"2rem"}} className="image" />
+      <div className="flex flex-row items-center justify-items-center w-1/3" >
+        <Button name= "다운로드"/>
+        <Button name = "공유하기"/>
       </div>
       </div>
     </div>
