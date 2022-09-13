cab9d19f1ba005cdf10063e7ec3614889d1af447
minjung-sys <cho9906@naver.com>
2022-09-14 02:11:21 +0900

    feat:Login modal 창 구현

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -1,4 +1,4 @@
-import React ,{useEffect,useState } from 'react';
+import React from 'react';
 import Header from '../components/Header';
 import LogoutBtn from '../components/LogoutBtn';
 import MyPageBtn from '../components/MyPageBtn';
