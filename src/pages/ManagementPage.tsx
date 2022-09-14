d5f8de801f71b5ad027a317e04c0edd0ff686768
Dong Seunghyeon <kj94gakt@naver.com>
2022-09-14 22:04:31 +0900

    style: border style 작업

--- src/pages/ManagementPage.tsx
+++ src/pages/ManagementPage.tsx
@@ -1,7 +1,11 @@
+/* eslint-disable import/order */
 import React from 'react';
 import Header from '../components/Header';
 import LogoutBtn from '../components/LogoutBtn';
 import MyPageBtn from '../components/MyPageBtn';
+
+/* React Icons */
+import { BsTrash } from 'react-icons/bs';
 
 function ManagementPage() {
   return (
@@ -19,11 +23,26 @@ function ManagementPage() {
       />
       <div className="flex ml-[4rem] text-3xl font-myy">Management</div>
       <div className="flex justify-center items-center">
-        <div className="justify-center items-center border-dashed border-8 rounded-3xl h-[30rem] w-[50rem] p-4 border-4">
-          <div className="flex justify-center items-center mt-[5rem]">
-            <div className="flex justify-center items-center border-solid border-2 rounded-2xl h-[5rem]">
-              <p>hello</p>
-            </div>
+        <div className="justify-center items-center border-solid border-8 rounded-3xl mt-[1rem] h-[35rem] w-[105rem] p-4">
+          <div>
+            <p className="flex text-4xl ml-[8rem] font-bmjua">
+              ID
+              <p className="ml-[15rem]">Email</p>
+              <p className="ml-[16rem]">Username</p>
+              <p className="ml-[13rem]">Date</p>
+              <p className="ml-[13rem]">Remove</p>
+            </p>
+          </div>
+          <div
+            style={{
+              flex: 1,
+              height: '5px',
+              backgroundColor: 'black',
+              marginBottom: '10px',
+            }}
+          />
+          <div className="flex justify-center items-center bg-gray-300 border-solid border-2 rounded-2xl h-[5rem]">
+            <p className="flex text-4xl font-bmjua">기능부분</p>
           </div>
         </div>
       </div>
