bc68788c05200a0e26d6e4c7d2315ec593993653
Dong Seunghyeon <kj94gakt@naver.com>
2022-09-14 17:54:24 +0900

    style: border style 작업

--- src/pages/ManagementPage.tsx
+++ src/pages/ManagementPage.tsx
@@ -18,6 +18,15 @@ function ManagementPage() {
         }}
       />
       <div className="flex ml-[4rem] text-3xl font-myy">Management</div>
+      <div className="flex justify-center items-center">
+        <div className="justify-center items-center border-dashed border-8 rounded-3xl h-[30rem] w-[50rem] p-4 border-4">
+          <div className="flex justify-center items-center mt-[5rem]">
+            <div className="flex justify-center items-center border-solid border-2 rounded-2xl h-[5rem]">
+              <p>hello</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
