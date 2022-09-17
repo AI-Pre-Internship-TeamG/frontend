80e89848d3da8f75f5cec02abd2ee21ebcc1b8fa
hyunwoo <ashyonu@gmail.com>
2022-09-18 03:24:34 +0900

    feat: add axios get img list

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Header from '../components/Header';
 import ImgList from '../components/imgList';
 import LogoutBtn from '../components/LogoutBtn';
@@ -10,8 +11,39 @@ export interface Data {
   date: string;
 }
 
+interface Response {
+  data: Data[];
+  meta: Meta;
+}
+
+export interface Meta {
+  has_next: boolean;
+  has_prev: boolean;
+  next_page: number;
+  page: number;
+  pages: number;
+  prev_page: number;
+}
+
 const MyPage = () => {
   const [datas, setDatas] = useState<Data[]>();
+  const [page, setPage] = useState<number>(5);
+  const [totalPages, setTotalPages] = useState(5);
+  const handlePages = (updatePage: number) => setPage(updatePage);
+
+  // const getData = async (page: number) => {
+  //   try {
+  //     const response = await axios.get('https://localhost:8000/api/v1/photos');
+  //     const imgData = response.data.images;
+  //     setTotalPages(response.data.meta.pages);
+  //   } catch(err) {
+  //     console.log(err);
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   getData(page); //
+  // }, [page]);
 
   useEffect(() => {
     setDatas(example);
