14b8d21a9849d4b01e4a4f63620809c0b475b968
Dong Seunghyeon <kj94gakt@naver.com>
2022-09-20 02:02:43 +0900

    feat: add React-Sketch-Canvas

--- src/pages/PictureEdit.tsx
+++ src/pages/PictureEdit.tsx
@@ -1,24 +1,27 @@
 /* eslint-disable import/order */
 /* eslint-disable react/jsx-curly-brace-presence */
-import React from 'react';
+import * as React from 'react';
 import Header from '../components/Header';
 import LogoutBtn from '../components/LogoutBtn';
 import MyPageBtn from '../components/MyPageBtn';
 import Btn from '../components/Btn';
 import { useLocation } from 'react-router-dom';
-/* React range */
+
+/* React canvas */
+import { ReactSketchCanvas } from 'react-sketch-canvas';
 
 /* React Icons */
 import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
 import { BsEraser } from 'react-icons/bs';
+import EditTool from '../components/EditTool';
 
 interface imgLocation {
-  data:string;
+  data: string;
 }
 export default function PictureEdit() {
   const location = useLocation();
   const state = location.state as imgLocation;
-  const {data} = state;
+  const { data } = state;
   return (
     <div className="bg-zinc-50">
       <LogoutBtn />
@@ -34,20 +37,25 @@ export default function PictureEdit() {
       />
       <div className="flex ml-[4rem] text-3xl font-myy">Edit</div>
       <div className="flex justify-center items-center">
-        {!data &&
-        <div className="justify-center items-center border-dashed border-8 rounded-3xl h-[30rem] w-[30rem] p-4 border-4">
-          <div className="justify-center text-4xl mt-[3rem] font-bmjua">
-            이미지 수정
+        {data && (
+          <div className="justify-center items-center border-dashed border-8 rounded-3xl h-[30rem] w-[30rem] p-4 ">
+            <img
+              className="flex mt-[0.6rem] w-auto h-auto max-h-[31rem]"
+              alt="Upload"
+              src={data}
+            />
+
+            <ReactSketchCanvas
+              strokeWidth={50}
+              strokeColor="blue"
+              className="w-[60rem] h-[60rem] border-dashed border-8 opacity-20 rounden-3xl stroke-4 stroke-cyan-500 relative -top-full"
+            />
           </div>
-        </div>}
-        {data &&
-          <div className="flex justify-center items-center  rounded-3xl h-[30rem] w-[30rem] p-4">
-             <img className = "flex w-auto h-auto max-h-[31rem]" alt = "Upload" src={data}/>
-          </div>
-        }
+        )}
       </div>
       <Btn name={'확정하기'} />
       <Btn name={'미리보기'} />
+      <EditTool />
       <div className="flex float-right mt-[6rem] mr-[26rem]">
         <div className="flex w-[15rem] h-[4rem] shadow-2xl justify-center items-center border-solid border-2 border-zinc-800 rounded-full">
           <FaAngleLeft className="flex w-[4rem] h-[3rem]" />
