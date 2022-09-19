89b278b94b57a9af79c52c9c5f0c3f3755ea3374
hyunwoo <ashyonu@gmail.com>
2022-09-20 01:07:47 +0900

    feat: change Img ratio

--- src/components/ImgCard.tsx
+++ src/components/ImgCard.tsx
@@ -9,8 +9,10 @@ export interface Data {
 
 const ImgCard = ({ data }: { data: Data }) => 
 (
-    <div className="flex flex-col w-1/4">
-        <img alt="Img" className="flex h-auto w-auto m-3" src={data.url} />
+    <div className="flex flex-col w-1/4 justify-end justify-self-center">
+        <div className="flex h-full w-auto h-auto m-3 object-center justify-center ">
+            <img alt="Img" className="flex h-auto w-auto object-contain " src={data.url} />
+        </div>
         <footer className="flex items-center justify-between leading-none p-2 md:p-4">
             <h1 className="text-lg">
                 {data.created_at}
