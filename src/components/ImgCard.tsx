f6817054b149b1750752b5928fe8b0cddf6deb7c
hyunwoo <ashyonu@gmail.com>
2022-09-20 00:34:54 +0900

    feat: add date param

--- src/components/ImgCard.tsx
+++ src/components/ImgCard.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { ReactComponent as Trash } from "../images/trash.svg";
 
-export interface data {
-    img_url: string;
-    date: string;
+export interface Data {
+    id: number;
+    url: string;
+    created_at: string;
 }
 
-const ImgCard = ({ data }: { data: data }) => (
+const ImgCard = ({ data }: { data: Data }) => 
+(
     <div className="flex flex-col w-1/4">
-        <img alt="Img" className="flex h-auto w-auto m-3" src={data.img_url} />
+        <img alt="Img" className="flex h-auto w-auto m-3" src={data.url} />
         <footer className="flex items-center justify-between leading-none p-2 md:p-4">
             <h1 className="text-lg">
-                {data.date}
+                {data.created_at}
             </h1>
             <button type="button">
                 <Trash className="hover:fill-red-500" />
--- src/components/imgList.tsx
+++ src/components/imgList.tsx
@@ -1,20 +1,15 @@
 import React from 'react';
-import ImgCard from './ImgCard';
+import ImgCard, {Data} from './ImgCard';
 
 // interface Response { 페이지네이션 기능 추가시 필요
 //     data: data[];
 //     meta: Meta;
 //   }
 
-export interface data {
-    img_url: string;
-    date: string;
-  }
-
-const ImgList = ({ data }: { data: data[] | undefined }) => (
+const ImgList = ({ data }: { data: Data[] | undefined }) => (
     <div className="container flex flex-row flex-wrap grow-0 box-border gap-y-5">
-        {data?.map(data => <ImgCard key={data.img_url} data={data} />)}
-    </div>        
+        {data?.map(data => <ImgCard key={data.id} data={data} />)}
+    </div> 
 )
 
 export default ImgList;
