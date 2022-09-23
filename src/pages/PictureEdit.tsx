6cb0b43ca7df8f355bacf78c9a64b2ca1a1d4086
hyunwoo <ashyonu@gmail.com>
2022-09-24 02:05:41 +0900

    feat: add axios content type

--- src/pages/PictureEdit.tsx
+++ src/pages/PictureEdit.tsx
@@ -74,6 +74,7 @@ export default function PictureEdit() {
     axios.post("http://localhost:8000/api/v1/photos/process/", data, 
     {
       headers: {
+      'Content-Type': 'multipart/form-data',
       'Authorization': `Bearer ${localStorage.getItem('token')}`
       }})
         .then((response:any) =>(
