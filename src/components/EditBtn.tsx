088a1ea43971d04f793ef156f411a0aafeddb59f
Dong Seunghyeon <kj94gakt@naver.com>
2022-09-23 11:54:17 +0900

    chore: add onClick props

--- src/components/EditBtn.tsx
+++ src/components/EditBtn.tsx
@@ -1,13 +1,23 @@
+/* eslint-disable @typescript-eslint/ban-types */
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Btn({ name }: { name: string }) {
+interface BtnProps {
+  name: string;
+  onClick: () => void;
+}
+// type Props = {
+//   onClick: Function;
+// };
+
+function EditBtn({ name, onClick }: BtnProps) {
   return (
     <div className="Btn">
       <div className="float-right mt-[4rem] mr-[4rem] font-sds text-4xl">
         <Link
-          to="/mypage"
+          to="/"
           style={{ color: 'inherit', textDecoration: 'inherit' }}
+          onClick={() => onClick()}
         >
           <p>{name}</p>
         </Link>
@@ -15,4 +25,4 @@ function Btn({ name }: { name: string }) {
     </div>
   );
 }
-export default Btn;
+export default EditBtn;
