2d0fea007a6a0183cd4ae7041b8a6ebef0b844a0
seeunjang <wisjang1@naver.com>
2022-09-25 18:49:43 +0900

    feat: 이미지 업로드 api 연동

--- src/pages/PictureUpload.tsx
+++ src/pages/PictureUpload.tsx
@@ -1,12 +1,16 @@
 import React,{useState,useRef,DragEvent,ChangeEvent,} from 'react';
+import axios from "axios";
+import {blob} from "stream/consumers";
 import { useNavigate} from 'react-router-dom';
 import Header from '../components/Header';
 import LogoutBtn from '../components/LogoutBtn';
 import MyPageBtn from '../components/MyPageBtn';
 
+
 function PictureUpload() {
   const navigate = useNavigate();
   const [fileImage,setFileImage] = useState<string>("");
+  const [uploadFile, setUploadFile] = useState(new Blob());
   const dropBoxArea = document.getElementById("drop-box");
   dropBoxArea?.addEventListener("dragenter",(event)=>{
     dropBoxArea?.classList.add("highlight")
@@ -15,19 +19,24 @@ function PictureUpload() {
     dropBoxArea?.classList.add("highlight")
   })
 
+
  
   
   const imageUpload = useRef<any>();
   const onClickImageUpload=()=>{
     imageUpload.current.click();
+
   };
   
   const saveFileImage = (e: ChangeEvent<HTMLInputElement>)=>{
     const target  = e.currentTarget;
     const files = (target.files as FileList)[0];
     const  blob = new Blob([files],{type:"images/jpg+png+jpeg"})
+    setUploadFile(blob);
     setFileImage(URL.createObjectURL(blob));
+
   };
+
   const dragOver = (e:DragEvent<HTMLDivElement>)=>{
     e.preventDefault();
 
@@ -37,20 +46,29 @@ function PictureUpload() {
     const Dropfile =( e.dataTransfer.files as FileList)[0];
     const Dropblob = new Blob([Dropfile],{type:"images/jpg+png+jpeg"})
     setFileImage(URL.createObjectURL(Dropblob))
-
-  
   }
 
+   const gotoFileEdit =(fileImage:string)=>{
 
-  const gotoFileEdit =(fileImage:string)=>{
-    if (fileImage){
-      navigate("/pictureedit",{state:fileImage})
-    }else{
-        alert('이미지를 업로드해주세요')
+        if (fileImage) {
+          const frm = new FormData()
+          frm.append('file', uploadFile)
+
+          axios.post("http://localhost:8000/api/v1/photos/upload/", frm ,
+              {headers: {
+            'Content-Type': 'multipart/form-data',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }})
+            .then((response:any) =>(
+              console.log("respone이 들어오면 진행할 것")
+          ));
+          navigate("/pictureedit", {state: fileImage})
+        } else {
+            alert('이미지를 업로드해주세요')
+        }
     }
-  }
 
-  return (
+    return (
     <div className="bg-zinc-50">
       <LogoutBtn />
       <MyPageBtn />
@@ -66,7 +84,9 @@ function PictureUpload() {
       <div className="flex ml-[4rem] text-3xl font-myy">Upload</div>
       <div className="flex justify-center items-center flex-col">
         {!fileImage &&
-        <div id = "drop-box" className="justify-center items-center border-dashed border-8 rounded-3xl h-[30rem] w-[30rem] p-4 border-4 hover:border-amber-600 focus:outline-none"
+        <div id = "drop-box" className=" justify-center items-center border-dashed border-8 rounded-3xl h-[20rem] w-[20rem] p-4 border-4  hover:border-amber-600 focus:outline-none
+            md:h-[30rem] md:w-[30rem]"
+      
             onDrop={onDropFiles}
             onDragOver={dragOver}
         >
@@ -75,27 +95,26 @@ function PictureUpload() {
               type = "file"
               style={{display:"none"}}
               />
-          <div className="flex justify-center items-center mt-[5rem]">
+          <div className="flex relative justify-center items-center mt-[5rem] top-[-20px] md:top-0">
             <img
               src="Images/Upload_Icon.png"
               alt="파일 아이콘"
-              width="200rem"
-              className="image"
+              className="w-[10rem] h-[10rem] md:w-[13rem] md:h-[13rem]"
             />
           </div>
           <div>
-            <div className="justify-center text-4xl mt-[3rem] font-bmjua">
+            <div className="relative justify-center top-[-35px] text-2xl mt-[3rem] font-bmjua md:text-4xl md:top-0">
             <p>이미지를 업로드 해주세요.</p>
-            </div>
           </div>
+        </div>
           
       </div>}
       {fileImage &&
-        <div className="flex justify-center items-center  rounded-3xl h-[30rem] w-[30rem] p-4">
-          <img className = "flex w-auto h-auto max-h-[31rem]" alt = "Upload" src={fileImage}/>
+        <div className="flex relative top-[15px] justify-center items-center  rounded-3xl h-[30rem] w-[30rem] p-4">
+          <img className = "flex w-auto h-auto max-h-[27rem] md:max-h-[31rem]" alt = "Upload" src={fileImage}/>
         </div>
       }
-      <div className="flex w-48 h-12 rounded-md font-bmjua text-2xl m-5 bg-orange-100 items-center justify-items-ceneter relative top-[2rem] hover:bg-orange-300">
+      <div className="flex w-44 h-12 rounded-md font-bmjua text-xl m-5 bg-orange-100 items-center justify-items-ceneter relative top-[2rem] hover:bg-orange-300 md:w-52 md:h-12 md:text-2xl">
         <button className="w-full" type ="button" onClick ={onClickImageUpload}>파일 업로드</button>
         <input
               name = "imageUpload"
@@ -106,8 +125,8 @@ function PictureUpload() {
       </div>
       </div>
       <div className="flex justify-center items-center">
-      <button type="button" className="flex justify-center mt-[3rem] font-sds text-6xl"
-        onClick={()=>gotoFileEdit(fileImage)}>
+      <button type="button" className="flex justify-center mt-[3rem] font-sds text-4xl md:text-6xl"
+    onClick={()=> gotoFileEdit(fileImage)}>
           <p>사 진 결 정</p>
       </button>
       </div>
