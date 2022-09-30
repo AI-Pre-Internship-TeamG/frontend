43723612e6bbe747ecae553257448dda23e9140f
Dong Seunghyeon <kj94gakt@naver.com>
2022-09-30 15:31:25 +0900

    chore: tailwind screen size 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,13 +11,13 @@ module.exports = {
         myy: ['"MYYeongnamnu"'],
       },
     },
-    screens :{
-      'sm':'640px',
-      'md':'768px',
-      'lg':'1024px',
-      'xl':'1280px',
-      '2xl':'1536px',
-    }
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
   },
   plugins: [],
 };
